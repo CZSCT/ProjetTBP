ad80c0959bdc91e3c324b174b7d967ee90509501
CZSCT <129674876+CZSCT@users.noreply.github.com>
2024-05-18 14:01:26 +0200

    main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  // Ouvrir le modal en cliquant sur le bouton
+  // Ouvrir le modal en cliquant sur le bouton.
   $("#openContactModal").click(function () {
     $("#contactModal").modal();
   });
